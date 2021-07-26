948d30fdf40dcc954171a78361cb9fd891b73d41
Zihad Bappy <zihadbappy@gmail.com>
2021-07-26 15:54:05 +0600

    3

--- public/chat.js
+++ public/chat.js
@@ -11,12 +11,12 @@ var userStream
 
 const iceServers = {
   iceServers: [
-    { urls: 'stun:stun.services.mozilla.com' },
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
-    { urls: 'stun:stun.ekiga.net' },
+    // { urls: 'stun:stun.services.mozilla.com' },
+    // { urls: 'stun:stun.l.google.com:19302' },
+    // { urls: 'stun:stun1.l.google.com:19302' },
+    // { urls: 'stun:stun3.l.google.com:19302' },
+    // { urls: 'stun:stun4.l.google.com:19302' },
+    // { urls: 'stun:stun.ekiga.net' },
     {
       urls: 'turn:18.119.113.154',
       username: 'zihad',
