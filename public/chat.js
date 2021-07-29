460c05dd87e7368ea03e7783a7bf32c6af812c34
Zihad Bappy <zihadbappy@gmail.com>
2021-07-30 02:47:42 +0600

    modified ip5

--- public/chat.js
+++ public/chat.js
@@ -18,9 +18,7 @@ const iceServers = {
     { urls: 'stun:stun4.l.google.com:19302' },
     { urls: 'stun:stun.ekiga.net' },
     {
-      urls: ['turn:3.19.7.137:3478?transport=udp',
-      'turn:3.19.7.137:3478?transport=tcp'
-    ],
+      urls: 'turn:3.140.252.7:3478',
       username: 'zihad',
       credential: '1234',
     },
