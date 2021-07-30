71cc0d67aacba03e8d437f08f2b9428133738176
Zihad Bappy <zihadbappy@gmail.com>
2021-07-30 14:11:17 +0600

    modified ip3332fe

--- public/chat.js
+++ public/chat.js
@@ -18,12 +18,7 @@ const iceServers = {
     // { urls: 'stun:stun4.l.google.com:19302' },
     // { urls: 'stun:stun.ekiga.net' },
     {
-      urls: 'turn:3.140.252.7:3478?transport=tcp',
-      username: 'zihad',
-      credential: '12345',
-    },
-    {
-      urls: 'turn:3.140.252.7:3478?transport=udp',
+      urls: 'turn:3.140.252.7:3478',
       username: 'zihad',
       credential: '12345',
     }
