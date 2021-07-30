56c5f8b3603819cde415becfccee2a0844bda8d2
Zihad Bappy <zihadbappy@gmail.com>
2021-07-30 14:14:58 +0600

    modified ip3332fee

--- public/chat.js
+++ public/chat.js
@@ -21,17 +21,7 @@ const iceServers = {
       urls: 'turn:3.140.252.7:3478',
       username: 'zihad',
       credential: '12345',
-    }
-    // {urls: "stun:stun.ideasip.com"},
-    // {urls: "stun:stun.iptel.org"},
-    // {urls: "stun:stun.rixtelecom.se"},
-    // {urls: "stun:stun.schlund.de"},
-    // {urls: "stun:stunserver.org"},
-    // {urls: "stun:stun.softjoys.com"},
-    // {urls: "stun:stun.voiparound.com"},
-    // {urls: "stun:stun.voipbuster.com"},
-    // {urls: "stun:stun.voipstunt.com"}
-  ],
+    }],
 }
 
 var nnn = 'Nig'
