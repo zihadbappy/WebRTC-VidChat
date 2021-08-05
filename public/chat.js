56209c047497fbb21ecd87792a8db7d52d17d08a
Zihad Bappy <zihadbappy@gmail.com>
2021-08-05 22:12:25 +0600

    kfdssd

--- public/chat.js
+++ public/chat.js
@@ -18,7 +18,7 @@ const iceServers = {
     { urls: 'stun:stun4.l.google.com:19302' },
     { urls: 'stun:stun.ekiga.net' },
     {
-      urls: 'turn:3.140.252.7:3478',
+      urls: 'turn:3.140.252.7:3479',
       username: 'zihad',
       credential: '1234',
     },
